1dd210f4cd2bb197d96904de0877d3db3b590d08
Szelor <szelor@users.noreply.github.com>
2019-04-14 07:23:30 +0200

    --

--- Chapter10/Final model.sql
+++ Chapter10/Final model.sql
@@ -1,16 +1,24 @@
 --Final model
+DELETE FROM [ML].[LendingClub].[Models]
+WHERE [Name] = 'RT Decision Forest';
+
+SELECT TOP 5 *
+FROM [ML].[LendingClub].[Models]
+ORDER BY [Test F-score] DESC;
+GO
+
 EXEC [LendingClub].[BuildRealtimeScoringModel]
 	@name = N'RT Decision Forest',
 	@input_query = N'SELECT * FROM [LendingClub].[vLoanStats] WHERE [is_train] = 1',
-	@n_tree = 20,
-	@m_try = 5, 
-	@max_depth = 5,
-	@minSplit = 200;
+	@n_tree = 38,
+	@m_try = 3, 
+	@max_depth = 12,
+	@minSplit = 100;
 GO
 
-SELECT TOP 5 [Name], LEN(Model)
+SELECT [Name], 1.*DATALENGTH (Model)/(1024*1024) AS [Model MB], [Train F-score]
 FROM [ML].[LendingClub].[Models]
-ORDER BY [Train F-score] DESC;
+WHERE nTree = 38 AND mTry = 3 AND maxDepth=12 AND minSplit=100;
 GO
 
 -- Native prediction
@@ -23,22 +31,9 @@ TRUNCATE TABLE [LendingClub].[LoanStatsPredictions]
 -- Log beginning of processing time
 INSERT INTO [LendingClub].[RunTimeStats] VALUES (@@SPID, GETDATE(),'Start scoring model RT Decision Forest')
 
-DECLARE @input_qry varchar(max);
 WITH d AS 
-	(SELECT id
-	  ,[is_bad]
-	  ,[grade]
-      ,[int_rate]
-	  ,[out_prncp_inv]
-      ,[policy_code]
-	  ,[installment]
-      ,[open_acc_6m]
-      ,[all_util]
-      ,[revol_util]
-      ,[total_rec_prncp]
-	  ,[bc_util]
-	  ,[percent_bc_gt_75]
-	  ,[home_ownership]
+	(SELECT id, is_bad, grade, int_rate, out_prncp_inv, policy_code, installment,
+	open_acc_6m, all_util, revol_util, total_rec_prncp, bc_util,home_ownership
 	FROM [LendingClub].[vLoanStats] WHERE [is_train] = 0)
 
 INSERT INTO [LendingClub].[LoanStatsPredictions] (id, actual, predicted) 
@@ -51,47 +46,51 @@ GO
 INSERT INTO [LendingClub].[RunTimeStats] VALUES (@@SPID, GETDATE(),'End scoring model RT Decision Forest')
 GO
 
-SELECT * 
+SELECT TOP 2 *
 FROM [LendingClub].[RunTimeStats]
+ORDER BY RunTime DESC;
 GO
 
+SELECT SUSER_SNAME()
+
 -- Score model
-SELECT *
-FROM [LendingClub].[LoanStatsPredictions]
+SELECT top 5 *
+FROM [LendingClub].[LoanStatsPredictions];
 GO
  
 DECLARE @tp INT, @fn INT, @fp INT, @tn INT
 SELECT @tp = COUNT(*) 
 	FROM [LendingClub].[LoanStatsPredictions] 
-	WHERE [actual] =1 and [predicted]=1 
+	WHERE [actual] =1 and [predicted]=1 ;
 
 SELECT @fn = COUNT(*) 
 	FROM [LendingClub].[LoanStatsPredictions] 
-	WHERE [actual] =1 and [predicted]=0
+	WHERE [actual] =1 and [predicted]=0;
 
 SELECT @fp = COUNT(*) 
 	FROM [LendingClub].[LoanStatsPredictions] 
-	WHERE [actual] =0 and [predicted]=1
+	WHERE [actual] =0 and [predicted]=1;
 
 SELECT @tn = COUNT(*) 
 	FROM [LendingClub].[LoanStatsPredictions] 
-	WHERE [actual] =0 and [predicted]=0
+	WHERE [actual] =0 and [predicted]=0;
 
-select @tp, @fn , @fp , @tn
+SELECT @tn AS TN, @fp AS FP;
+SELECT @fn AS FN, @tp AS TP;
 
 DECLARE @accuracy DECIMAL(3,2) = (@tp + @tn)*1. / (@tp + @fn + @fp + @tn)
 DECLARE @precision DECIMAL(3,2) = @tp*1. / (@tp + @fp)
 DECLARE @recall DECIMAL(3,2) = @tp*1. / (@tp + @fn)
 DECLARE @fscore DECIMAL(3,2)  = 2. * (@precision * @recall) / (@precision + @recall)
 
-SELECT @accuracy, @precision, @recall, @fscore
+SELECT @accuracy AS Accuracy, @precision AS Precision, @recall AS Recall, @fscore AS [F-score];
 
 UPDATE [LendingClub].[Models]
 SET [Test F-score] = @fscore
-WHERE [Name] = 'RT Decision Forest'
+WHERE [Name] = 'RT Decision Forest';
 GO
 
-SELECT TOP 10 [Name],[nTree],[mTry],[maxDepth],[minSplit],[Train F-score],[Test F-score] 
+SELECT [Name],[Train F-score],[Test F-score] 
 FROM [LendingClub].[Models]
-ORDER BY [Test F-score] DESC;
+WHERE nTree = 38 AND mTry = 3 AND maxDepth=12 AND minSplit=100;
 GO
--- Chapter10/WhatIf.sql
+++ Chapter10/WhatIf.sql
@@ -4,29 +4,66 @@ DECLARE @model varbinary(max)
 SELECT @model = [model] from [LendingClub].[Models] 
 	WHERE [name] = 'RT Decision Forest';
 
+TRUNCATE TABLE [LendingClub].[LoanStatsPredictions];
+
+WITH d AS 
+	(SELECT id, is_bad, grade, int_rate, out_prncp_inv, policy_code, installment,
+	open_acc_6m, all_util, revol_util, total_rec_prncp, bc_util,home_ownership
+	FROM [LendingClub].[LoanStats] WHERE [is_train] = 0)
+INSERT INTO [LendingClub].[LoanStatsPredictions] (id, actual, predicted) 
+SELECT id, is_bad, is_bad_Pred
+FROM PREDICT(MODEL = @model, DATA = d)
+WITH ([0_prob] float, [1_prob] float, is_bad_Pred nvarchar(max)) p;
+GO
+
+DECLARE @model varbinary(max)
+SELECT @model = [model] from [LendingClub].[Models] 
+	WHERE [name] = 'RT Decision Forest';
+
 TRUNCATE TABLE [LendingClub].[LoanPredictionsWhatIf];
 
-DECLARE @input_qry varchar(max);
 WITH d AS 
-	(SELECT id
-	  ,[is_bad]
-	  ,[grade]
-      ,[int_rate] + 5 AS [int_rate]
-	  ,[out_prncp_inv]
-      ,[policy_code]
-	  ,[installment]
-      ,[open_acc_6m]
-      ,[all_util]
-      ,[revol_util]
-      ,[total_rec_prncp]
-	  ,[bc_util]
-	  ,[percent_bc_gt_75]
-	  ,[home_ownership] 
-	  FROM [LendingClub].[vLoanStats] 
+	(SELECT id, is_bad, grade, int_rate+9 as int_rate, out_prncp_inv, policy_code, installment,
+	open_acc_6m, all_util, revol_util, total_rec_prncp, bc_util,home_ownership 
+	  FROM [LendingClub].[LoanStats] 
 	  WHERE [is_train] = 0)
-
 INSERT INTO [LendingClub].[LoanPredictionsWhatIf] (id, actual, predicted) 
 SELECT id, is_bad, is_bad_Pred
 FROM PREDICT(MODEL = @model, DATA = d)
-WITH ([0_prob] float, [1_prob] float, is_bad_Pred nvarchar(max)) p ;
+WITH ([0_prob] float, [1_prob] float, is_bad_Pred nvarchar(max)) p;
 GO
+
+SELECT COUNT(*)
+FROM [LendingClub].[LoanStatsPredictions] as P
+JOIN [LendingClub].[LoanPredictionsWhatIf] as I
+ON P.id=I.id
+WHERE P.predicted<>I.predicted;
+GO
+
+SELECT AVG(1.*predicted) AS Average, STDEV(predicted) AS StDeviation
+FROM [LendingClub].[LoanPredictionsWhatIf]
+UNION ALL
+SELECT AVG(1.*predicted), STDEV(predicted)
+FROM [LendingClub].[LoanStatsPredictions];
+GO
+
+SELECT loan_status,st.addr_state,CASE WHEN p.predicted = 1 THEN 'High'
+	ELSE 'Low' END AS [ChargeOffProbability], 
+	COUNT(*) as [Number of Loans], SUM(funded_amnt) AS [Loan Amount]
+FROM [LendingClub].[LoanStats] st
+INNER JOIN [LendingClub].[LoanStatsPredictions] p
+on st.id = p.id
+GROUP BY loan_status,st.addr_state, CASE WHEN p.predicted = 1 THEN 'High'
+	ELSE 'Low' END;
+GO
+
+SELECT loan_status,st.addr_state,CASE WHEN wi.predicted = 1 THEN 'High'
+	ELSE 'Low'
+	END AS [ChargeOffProbability], 
+	COUNT(*) as [Number of Loans], SUM(funded_amnt) as [Loan Amount]
+FROM [LendingClub].[LoanStats] st
+INNER JOIN [LendingClub].[LoanPredictionsWhatIf] wi
+on st.id = wi.id
+GROUP BY loan_status,st.addr_state, CASE WHEN wi.predicted = 1 THEN 'High'
+	ELSE 'Low'
+	END;
